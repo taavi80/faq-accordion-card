ee8eb311f46688abf280e6f933f4c1487020fa4b
taavi80 <123880364+taavi80@users.noreply.github.com>
2023-06-12 18:30:07 +0300

    updated index.html script.js style.css

--- script.js
+++ script.js
@@ -7,8 +7,29 @@ for (let i = 0; i < questions.length; i++) {
   const arrowImg = question.querySelector(".arrow-img");
 
   question.addEventListener("click", function () {
-    answer.classList.toggle("show");
-    h2.classList.toggle("active");
-    arrowImg.classList.toggle("rotate");
+    const isActive = h2.classList.contains("active");
+
+    // Remove active class from all h2 elements
+    const questionsH2 = document.querySelectorAll(".question h2");
+    questionsH2.forEach((h2) => {
+      h2.classList.remove("active");
+    });
+
+    // Hide all answer elements
+    const allAnswers = document.querySelectorAll(".answer");
+    allAnswers.forEach((answer) => {
+      answer.classList.remove("show");
+    });
+
+    // Reset arrow images to not clicked state
+    const allArrowImg = document.querySelectorAll(".arrow-img");
+    allArrowImg.forEach((arrowImg) => {
+      arrowImg.classList.remove("rotate");
+    });
+
+    // Toggle classes for the clicked question
+    answer.classList.toggle("show", !isActive);
+    h2.classList.toggle("active", !isActive);
+    arrowImg.classList.toggle("rotate", !isActive);
   });
 }
